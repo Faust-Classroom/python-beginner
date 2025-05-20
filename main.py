0bd392d53b98cf4aa7a1845c2b18fa33116f4b89
Eric <test@email.com>
2025-05-20 10:25:52 +0200

    start set-up

---
+++ main.py
@@ -0,0 +1,6 @@
+# TODO: Write a function that returns the sum of an array -> array_sum
+
+def array_sum(nums):
+  
+    
+    return 0
---
+++ tests/test_main.py
@@ -0,0 +1,6 @@
+from main import array_sum
+
+def test_basic():
+    assert array_sum([1, 2, 3]) == 6
+    assert array_sum([-6, 10, 6]) == 10
+    
